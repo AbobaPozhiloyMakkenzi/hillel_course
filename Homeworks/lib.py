379fd7eac857297d74d854085d977beea363d92f
AbobaPozhiloyMakkenzi <naddison123321656@gmail.com>
2023-06-13 14:28:18 +0300

    done

--- Homeworks/lib.py
+++ Homeworks/lib.py
@@ -1,4 +1,5 @@
 import time
+import random
 machine_win_counter = 0
 man_win_counter = 0
 
@@ -47,3 +48,26 @@ def logics(el1,el2,pick):
         machine_goals()
     if el1 == el2:
         print('It`s a draw!')
+
+@time_counter
+def game():
+    while machine_win_counter < 3 and man_win_counter < 3:
+        """
+        engine of the game 
+        data source(players choice, machine element randomizer), game rules and user input control.
+        """
+        players_choice = input('rock, paper, scissors, 3.... 2..... 1....').strip().lower()
+        game_elements = ['paper', 'rock', 'scissors']
+        pick = random.choice(game_elements)
+
+        if players_choice not in game_elements:
+            break  # paper beats rock, scissors beat paper, rock beats scissors
+
+        logics(players_choice, pick,pick)
+
+        question = input('do you want to continue?')
+        if question != 'yes':
+
+            break
+
+
