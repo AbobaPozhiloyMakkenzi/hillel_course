862bb16ce118b22d4fb65dfe842f013ebcecf0ef
AbobaPozhiloyMakkenzi <naddison123321656@gmail.com>
2023-07-08 18:21:09 +0300

    Project

--- Homeworks/HM14.2.py
+++ Homeworks/HM14.2.py
@@ -1,13 +1,57 @@
-
+import json
+import matplotlib.pyplot as plt
 import requests
-my_link = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
-res = requests.request('GET', my_link)
-responce_json = res.json()
+dates = []
+values = []
+currencies = ['EUR',"USD"]
+
+while True:
+
+    while True:
+        pick = input('pick the currency!').upper().strip()
+        if not isinstance(pick,str):
+            break
+        elif pick not in currencies:
+            print('wrong name of the currency')
+        else:
+
+            date = int(input('pick the date!'))
+            if not isinstance(date,int):
+                break
+            iter_num = 2024 - date
 
 
-with open('currencies.txt', 'w') as file:
-    for i in range(len(responce_json)):
-        data = responce_json[i]
-        name, rate = data['txt'], data['rate']
-        file.write(str(f'{i+1}) {name} to UAH currency: {rate} \n'))
+            for i in range(iter_num):
+                link = f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json&valcode={pick}&date={date+i}0101'
+                dates.append(int(date+i))
+                try:
+                    res = requests.request('GET', link)
+                except Exception as e:
+                    print(e)
+                else:
+                    responce_json = res.json()
+                    with open('currencies.txt', 'a') as file:
+                        for i in range(len(responce_json)):
+                            data = responce_json[i]
+                            name, rate = data['txt'], data['rate']
+                            values.append(data['rate'])
+                            file.write(str(f'{data["exchangedate"]}) {name} to UAH currency: {rate} \n'))
+        if input('Wanna get a report?') == 'yes':
+            break
+
+    fig, ax = plt.subplots()
+
+    ax.plot(dates, values)
+    ax.set_title = 'Currencies'
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Rate')
+
+    ax.scatter(dates, values)
+
+    plt.show()
+
+    decision = input('wanna continue?').strip().lower()
+    if decision != 'yes':
+        break
+
 
